fa96618032b710ef34b2409e8547308cc3d0c96f
nazeeya693 <banunazeeya693@gmail.com>
2026-04-29 14:57:42 +0530

    Add files via upload
    Initial upload of fashion look_book project

---
+++ script.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
